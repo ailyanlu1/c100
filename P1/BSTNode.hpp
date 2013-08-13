9ac9d09aa00e299193e09cca13eec521886eb340
Jay Dey <cs100vaj@ieng6-202.ucsd.edu>
2013-08-12 18:12:51 -0700

    what the poop is going on, recursionized successor, find verified quit

--- P1/BSTNode.hpp
+++ P1/BSTNode.hpp
@@ -29,15 +29,13 @@ public:
 
   /** BSTNode Destructor
    * 
-   */
+   */ //TODO
   ~BSTNode()
   {
-    delete (*this)->left;
-    delete (*this)->right;
-    delete (*this);
-    (*this)->left = NULL;
-    (*this)->right = NULL;
-    (*this) = NULL;
+    delete this->left;
+    delete this->right;
+    this->left = NULL;
+    this->right = NULL;
   }
 
   /** Return the inorder successor of this BSTNode in a BST,
@@ -48,8 +46,9 @@ public:
    *  or nullptr if there is none.
    */ // TODO
   BSTNode<Data>* successor() {
-    BSTNode<Data>* tmpNode1 = new BSTNode<Data>;
-    BSTNode<Data>* tmpNode2 = new BSTNode<Data>;
+  /*
+    BSTNode<Data>* tmpNode1 = this;
+    BSTNode<Data>* tmpNode2 = this;
     //case 1: right child of current node exists
     if (this->right) {
       tmpNode1 = this->right;
@@ -59,17 +58,59 @@ public:
     }
     //case 2: no right child
     else {
-      tmpNode2 = this;
       while((tmpNode1 = tmpNode2->parent)) { //climb
         //when you find the parent has left decendent tmpNode2, stop.
         if(tmpNode1->left == tmpNode2){          
           break;
         }
         tmpNode2 = tmpNode1;
+	if (tmpNode1->parent == NULL){
+          return NULL;
+	}
       }
     }
     return tmpNode1;
+    */
+    if (this == NULL)
+      return NULL;
+
+    //has right child, which might have left children
+    if (this->right != NULL)
+      return helperRight(this->right);
+
+    //no right child, but parent
+    if ( (this->right == NULL) && (this->parent) )
+      return helperParent(this->parent, this);
+
+    return NULL;
   }
+
+  /*
+   * checks for left children, otherwise return right child
+   * leftmost child would be successor
+   */
+  BSTNode<Data>* helperRight(BSTNode<Data> * node)
+  {
+    if (node->left != NULL)
+      helperRight(node->left);
+    
+    return node;
+  }
+
+  BSTNode<Data>* helperParent(BSTNode<Data>* parentN, BSTNode<Data>* original)
+  {
+    //parent is successor
+    if ((parentN->data) > original->data)
+      return parentN;
+
+    //original = end
+    else if ((parentN->parent == NULL) && (original->data > parentN->data))
+      return NULL;
+
+    //
+    return helperParent(parentN->parent, original);
+  }
+
 }; 
 
 /** Overload operator<< to insert a BSTNode's fields in an ostream. */
