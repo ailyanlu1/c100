1770a6c00883f2ea7c1000f028d95c7f65e20a85
Joshua E Yuen <cs100vbc@acs-cseb240-17.ucsd.edu>
2013-08-15 12:23:16 -0700

    added protected priority var

--- P2/RSTNode.hpp
+++ P2/RSTNode.hpp
@@ -7,15 +7,21 @@
 #ifndef RSTNODE_HPP
 #define RSTNODE_HPP
 #include "BSTNode.hpp"
+#include <stdlib.h>
 
 template <typename Data>
 class RSTNode : public BSTNode<Data>
 {
+
+protected:
+
+  unsigned int priority;
+
 public:
   //TODO: implement this constructor
   RSTNode(Data const & d) : BSTNode<Data>(d)
   {
-    
+    priority = rand();
   }
 
 };
