28b11bbd91d75b4adf49d578cf71df0ff6356547
Jay Dey <cs100vaj@ieng6-202.ucsd.edu>
2013-08-15 16:05:48 -0700

    Created and started working on benchmarking program

---
+++ P2/benchtree.cpp
@@ -0,0 +1,50 @@
+#include "RST.hpp"
+#include "countint.hpp"
+#include <cmath>
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <set>
+
+using namespace std;
+
+/**
+ * A simple benchmarking program for RST.
+ */
+static void show_usage(std::string name)
+{
+  std::cerr <<
+}
+
+int main (int argc, char** argv) {
+
+  int i;
+  if(argc <4) {
+    show_usage(argv[0]);
+    return 1;
+  }
+  if(argv[1] == "bst"){
+
+  }
+  else if (argv[1] == "rst"){
+
+  }
+  else if (agv[1] == "set"){
+
+  }
+  else if(argv[2] == "sorted"){
+
+  }
+  else if(argv[2] == "shuffled"){
+
+  }
+  else if(isdigit(argv[3])){
+    i = atoi (argv
+  }
+  else{
+    show_usage(std::string name);
+  }
+
+  ;
+
+}
