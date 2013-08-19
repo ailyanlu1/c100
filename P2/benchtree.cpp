9639a5baf80443a5a2ac420e3281b5613c4d010d
Joshua E Yuen <cs100vbc@ieng6-202.ucsd.edu>
2013-08-19 11:36:39 -0700

    slow and steady

--- P2/benchtree.cpp
+++ P2/benchtree.cpp
@@ -11,40 +11,73 @@ using namespace std;
 /**
  * A simple benchmarking program for RST.
  */
-static void show_usage(std::string name)
+static void show_usage()
 {
-  std::cerr <<
+  std::cerr << "./benchtree [rst, bst, set] [sorted, shuffled]"
+               " [maximum tree size] [number of runs]" << endl;
 }
 
-int main (int argc, char** argv) {
-
-  int i;
-  if(argc <4) {
-    show_usage(argv[0]);
+int main (int argc, char** argv) 
+{
+  if(argc != 5)
+  {
+    show_usage();
     return 1;
   }
-  if(argv[1] == "bst"){
 
+  // parse arg 1: type of structure
+  if(strcmp(argv[1], "bst") == 0)
+  {
+    BST<countint> * tree = new BST<countint>();
   }
-  else if (argv[1] == "rst"){
-
+  else if (strcmp(argv[1], "rst") == 0)
+  {
+    RST<countint> * tree = new RST<countint>();
   }
-  else if (agv[1] == "set"){
-
+  else if (strcmp(argv[1], "set") == 0)
+  {
+    std::set<countint> * tree = new std::set<countint>();
   }
-  else if(argv[2] == "sorted"){
-
-  }
-  else if(argv[2] == "shuffled"){
-
-  }
-  else if(isdigit(argv[3])){
-    i = atoi (argv
-  }
-  else{
-    show_usage(std::string name);
+  else
+  {
+    show_usage();
+    return 1;
   }
 
-  ;
+  //parse arg 2: keys in sorted or randomized order
+  if(strcmp(argv[2], "sorted") == 0)
+  {
 
+  }
+  else if(strcmp(argv[2], "shuffled") == 0)
+  {
+
+  }
+  else
+  {
+    show_usage();
+    return 1;
+  }
+
+  //parse arg 3: maximum size of tree
+  if(isdigit(argv[3]))
+  {
+    int n = atoi (argv[3]);
+  }
+  else
+  {
+    show_usage();
+    return 1;
+  }
+
+  //parse arg 4: number of runs
+  if (isdigit(argv[4]))
+  {
+    int r = atoi( argv[4] );
+  }
+  else
+  {
+    show_usage();
+    return 1;
+  }
 }
