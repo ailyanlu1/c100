432987d3d4db444238f19074e3ffe2fce408982f
Joshua E Yuen <cs100vbc@acs-cseb240-20.ucsd.edu>
2013-08-19 22:02:29 -0700

    reconfigured

--- P2/benchtree.cpp
+++ P2/benchtree.cpp
@@ -23,24 +23,25 @@ using namespace std;
 static void show_usage()
 {
   std::cerr << "./benchtree [rst / bst / set] [sorted / shuffled]"
-               " [maximum tree size] [number of runs]" << endl;
+               " [maximum tree size] [number of runs]" << std::endl;
 }
 
 int main (int argc, char** argv) 
 {
+  //incorrect usage
   if(argc != 5)
   {
     show_usage();
     return 1;
   }
 
-  // parse arg 1: type of structure
-  if(strcmp(argv[1], "bst") == 0)
-    BST<countint> * bam = new BST<countint>();
-  else if (strcmp(argv[1], "rst") == 0)
-    RST<countint> * bam = new RST<countint>();
-  else if (strcmp(argv[1], "set") == 0)
-    std::set<countint> * bam = new std::set<countint>();
+  //parse arg 2: keys in sorted or randomized order
+  int sorted = 0;
+  int shuffled = 0;
+  if(strcmp(argv[2], "sorted") == 0)
+    sorted = 1;
+  else if(strcmp(argv[2], "shuffled") == 0)
+    shuffled = 1;
   else
   {
     show_usage();
@@ -56,29 +57,184 @@ int main (int argc, char** argv)
   }
 
   //parse arg 4: number of runs
-  int r = atoi( argv[4] );
-  if (r == 0)
+  int runs = atoi( argv[4] );
+  if (runs == 0)
   {
     show_usage();
     return 1;
   }
 
 
-  std::vector<countint> v;
-  v.clear();
-
-  //parse arg 2: keys in sorted or randomized order
-  if(strcmp(argv[2], "sorted") == 0)
+  std::cout<< "# Benchmarking average number of comparisons for successful find"
+              "\n# Data structure: " << argv[1] << "\n"
+	      "# Data: " << argv[2] << "\n"
+	      "# N is powers of 2, minus 1, from 1 to " << n << "\n"
+	      "# Averaging over " << runs << "runs for each N \n"
+	      "#\n" 
+	      "# N \t avgcomps \t stdev\n" << std::endl;
+  
+  /*
+   * parse arg 1: type of data structure
+   * bst, rst, set
+   */
+  if(strcmp(argv[1], "bst") == 0)
   {
-    for (int i=0; i<n; ++i)
+    BST<countint> * bam = new BST<countint>();
+
+    std::vector<countint> v;
+    v.clear();
+ 
+    //populate vector
+    for (int i=0; i<n; i++)
       v.push_back(i);
+
+    //shuffle if necessary
+    if (shuffled == 1)
+      std::random_shuffle(v.begin(), v.end());
+
+    //insert keys into structure
+    std::vector<countint>::iterator vit = v.begin();
+    std::vector<countint>::iterator ven = v.end();
+  
+    for (vit = v.begin(); vit != ven; ++vit)
+      bam->insert(*vit);
+
+    //comparisons
+    int x = 1; 
+    while (x <= n)
+    {
+      double tot_avg = 0;
+      double tot_sq_avg = 0;
+
+      countint::clearcount();
+      for (int a = 0; a < runs; a++)
+      {
+        for (vit = v.begin(); vit != ven; ++vit)
+        {
+          bam->find(*vit);
+
+          double avgcomps = countint::getcount() / (double)x;
+
+          tot_avg = tot_avg + avgcomps;
+          tot_sq_avg = tot_sq_avg + pow(avgcomps, 2);
+        }
+      }
+      tot_avg = tot_avg / (double)runs;
+      tot_sq_avg = tot_sq_avg / (double)runs;
+
+      double stdev = sqrt(abs(tot_sq_avg - (pow(tot_avg, 2))));
+
+      std::cout << x << " \t " << tot_avg << " \t " << stdev << "\n" << std::endl;
+    
+      x = ((++x) ^ 2) - 1;
+    }
   }
-  else if(strcmp(argv[2], "shuffled") == 0)
+  
+  else if (strcmp(argv[1], "rst") == 0)
   {
-    for (int i=0; i<n; ++i)
+    RST<countint> * ram = new RST<countint>();
+
+    std::vector<countint> v;
+    v.clear();
+ 
+    //populate vector
+    for (int i=0; i<n; i++)
       v.push_back(i);
 
-    std::random_shuffle(v.begin(), v.end());
+    //shuffle if necessary
+    if (shuffled == 1)
+      std::random_shuffle(v.begin(), v.end());
+
+    //insert keys into structure
+    std::vector<countint>::iterator vit = v.begin();
+    std::vector<countint>::iterator ven = v.end();
+  
+    for (vit = v.begin(); vit != ven; ++vit)
+      ram->insert(*vit);
+
+    //comparisons
+    int x = 1; 
+    while (x <= n)
+    {
+      double tot_avg = 0;
+      double tot_sq_avg = 0;
+
+      countint::clearcount();
+      for (int a = 0; a < runs; a++)
+      {
+        for (vit = v.begin(); vit != ven; ++vit)
+        {
+          ram->find(*vit);
+
+          double avgcomps = countint::getcount() / (double)x;
+
+          tot_avg = tot_avg + avgcomps;
+          tot_sq_avg = tot_sq_avg + pow(avgcomps, 2);
+        }
+      }
+      tot_avg = tot_avg / (double)runs;
+      tot_sq_avg = tot_sq_avg / (double)runs;
+
+      double stdev = sqrt(abs(tot_sq_avg - (pow(tot_avg, 2))));
+
+      std::cout << x << " \t " << tot_avg << " \t " << stdev << "\n" << std::endl;
+    
+      x = ((++x) ^ 2) - 1;
+    }
+  }
+
+  else if (strcmp(argv[1], "set") == 0)
+  {
+    std::set<countint> * sam = new std::set<countint>();
+
+    std::vector<countint> v;
+    v.clear();
+ 
+    //populate vector
+    for (int i=0; i<n; i++)
+      v.push_back(i);
+
+    //shuffle if necessary
+    if (shuffled == 1)
+      std::random_shuffle(v.begin(), v.end());
+
+    //insert keys into structure
+    std::vector<countint>::iterator vit = v.begin();
+    std::vector<countint>::iterator ven = v.end();
+  
+    for (vit = v.begin(); vit != ven; ++vit)
+      sam->insert(*vit);
+
+    //comparisons
+    int x = 1; 
+    while (x <= n)
+    {
+      double tot_avg = 0;
+      double tot_sq_avg = 0;
+
+      countint::clearcount();
+      for (int a = 0; a < runs; a++)
+      {
+        for (vit = v.begin(); vit != ven; ++vit)
+        {
+          sam->find(*vit);
+
+          double avgcomps = countint::getcount() / (double)x;
+
+          tot_avg = tot_avg + avgcomps;
+          tot_sq_avg = tot_sq_avg + pow(avgcomps, 2);
+        }
+      }
+      tot_avg = tot_avg / (double)runs;
+      tot_sq_avg = tot_sq_avg / (double)runs;
+
+      double stdev = sqrt(abs(tot_sq_avg - (pow(tot_avg, 2))));
+
+      std::cout << x << " \t " << tot_avg << " \t " << stdev << "\n" << std::endl;
+    
+      //x^2 + 1
+      x = ((++x) ^ 2) - 1;
+    }
   }
   else
   {
@@ -86,49 +242,6 @@ int main (int argc, char** argv)
     return 1;
   }
 
-  std::vector<countint>::iterator vit = v.begin();
-  std::vector<countint>::iterator ven = v.end();
-  
-  for (vit = v.begin(); vit != ven; ++vit)
-    bam.insert(*vit);
-
-  std::cout<< "# Benchmarking average number of comparisons for successful find"
-              "\n# Data structure: " << argv[1] << "\n"
-	      "# Data: " << argv[2] << "\n"
-	      "# N is powers of 2, minus 1, from 1 to " << n << "\n"
-	      "# Averaging over " << r << "runs for each N \n"
-	      "#\n" 
-	      "# N \t avgcomps \t stdev\n" << endl;
-  
-  //comparisons
-  int x = 1;
-  double tot_avg = 0;
-  double tot_sq_avg = 0;
-  
-  while (x <= n)
-  {
-    countint::clearcount();
-    for (int a = 0; a < r; a++)
-    {
-      for (vit = v.begin(); vit != ven; ++vit)
-      {
-        bam.find(*vit);
-
-        double avgcomps = countint::getcount() / (double)n;
-
-        tot_avg = tot_avg + avgcomps;
-        tot_sq_avg = tot_sq_avg + pow(avgcomps, 2);
-      }
-    }
-    tot_avg = tot_avg / r;
-    tot_sq_avg = tot_sq_avg / r;
-
-    double stdev = sqrt((tot_sq_avg - (pow(tot_avg, 2))));
-
-    std::cout << x << " \t " << tot_avg << " \t " << stdev << "\n" << endl;
-    
-    x = ((x + 1) * 2) - 1;
-  }
-
-  return 1;
+  //SUCCESS
+  return 0;
 };
