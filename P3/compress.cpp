c83c821bb569b0c9a4ef75c8e2b17a6b3d3701ca
Joshua E Yuen <cs100vbc@acs-cseb250-23.ucsd.edu>
2013-08-22 13:54:30 -0700

    let's get it on

---
+++ P3/compress.cpp
@@ -0,0 +1,16 @@
+/*
+ * P3
+ * compress.cpp
+ * main driver for compression process
+ * Author: Jay Dey cs100vaj
+ * Author: Joshua Yuen cs100vbc
+ */
+
+#include <fstream>
+
+using namespace std;
+
+int main( int argc, char* argv[] )
+{
+
+}
