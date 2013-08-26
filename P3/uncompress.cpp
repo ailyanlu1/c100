f62145fe3f067610eec1a742ddb9409f44eef8d7
Joshua E Yuen <cs100vbc@ieng6-202.ucsd.edu>
2013-08-25 22:37:16 -0700

    completed, untested

---
+++ P3/uncompress.cpp
@@ -0,0 +1,112 @@
+/*
+ * P3
+ * uncompress.cpp
+ * Main driver for decopmression process
+ *
+ * Author: Jay Dey cs100vaj
+ * Author: Joshua Yuen cs100vbc
+ */
+
+#include <fstream>
+#include <iostream>
+#include "HCTree.cpp"
+
+using namespace std;
+
+int main( int argc, char* argv[] )
+{
+  if( argc != 3 )
+  {
+    cerr << "Incorrect arguments." << endl;
+    cerr << "Usage: ./uncompress infile outfile" << endl;
+    return 1;
+  }
+
+  cout << "Reading header from file: " << argv[1] << "... " << end;
+
+  vector<int> freq(256);
+  ifstream in;
+  in.open( argv[1], ios::binary );
+
+  int symbols = 0, bytesize = 0, compsize = 0;
+  bool empty = true;
+  char ch;
+  int num;
+
+  if( in.is_open() && in.good() )
+  {
+    for( int i=0; i<freq.size(); i++ )
+    {
+      in >> num;
+      freq[i] = num;
+      bytesize = bytesize + num;
+    }
+  }
+
+  cout << "DONE." << endl;
+
+  /* report outfile stats */
+  for( int i=0; i<freq.size(); i++ )
+    if( freq[i] != 0 )
+      symbols++;
+
+  if( symbols == 0 )
+    empty = true;
+
+  cout << "Uncompressed file will have " << symbols << " unique symbols and"
+          " size " << bytesize << " bytes." << endl;
+
+  /* build Huffman tree */
+  cout << "Building Huffman tree... " << endl;
+
+  HCTree tree;
+  if( !empty )
+    tree.build( freq );
+
+  cout << "DONE." << endl;
+
+  /* Write uncompressed data to outfile */
+  BitInputStream is = BitInputStream( in );
+  ofstream out;
+  out.open( argv[2], ios::binary );
+
+  cout << "Writing to file: " << argv[2] << "... " << endl;
+
+  if ( !empty )
+    for( int i=0; i<bytesize; i++ )
+      out << tree.decode( is );
+
+  cout << "DONE." << endl;
+
+  in.close();
+  out.close();
+
+  in.clear();
+  in.open( argv[1], ios::binary );
+
+  /* infile stats */
+  while( in.is_open() )
+  {
+    ch = in.get();
+
+    if( !in.good() ) break;
+
+    compsize++;
+  }
+  if( !in.eof() )
+  {
+    cerr << "There was a problem, sorry." << endl;
+    return -1;
+  }
+
+  in.close();
+  double ratio = 0;
+  if( bytesize != 0 && compsize != 0 )
+    ratio = (double)bytesize / (double)compsize;
+
+  cout << "Uncompression ratio: " << ratio << endl;
+
+  return 0;
+}
+
+//EOF uncompress.cpp
