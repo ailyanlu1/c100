3a459577e13abb4c0f3856a45285d2f7e0cea70d
Jay Dey <cs100vaj@ieng6-202.ucsd.edu>
2013-08-26 22:00:37 -0700

    Wrapped up HCTree build function

--- P3/HCTree.cpp
+++ P3/HCTree.cpp
@@ -12,11 +12,21 @@ void HCTree::build(const vector<int>& freqs) {
   for (int i = 0; i < 256, i++){ //convert vector to HCNodes
     HCNode* temp = new HCNode(freqs[i], i, 0, 0, 0);
     leaves[i] = temp; //create a new vector to look up the pointers
-                      //for those HCNodes based on bit values
+                      //for those HCNodes based on bit value
   }
 
-std::priority_queue< HCNode, leaves, HCNodePtrComp> pQueue;
+  std::priority_queue< HCNode, leaves, HCNodePtrComp> pQueue;
 
+  while(pQueue.size() != 1){
+    HCNode* temp1 = pQueue.top();
+    pQueue.pop();
+    HCNode* temp2 = pQueue.top();
+    pQueue.pop();
+    HCNode* tempR = new HCNode(temp1.count + temp2.count, temp1, temp2, 0);
+    temp1.p = tempR;
+    temp2.p = tempR;
+    pQueue.push(tempR);
+  }
 
 
 }
