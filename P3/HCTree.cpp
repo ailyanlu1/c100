466267fbaadc592a0c0cc22fa3ae8c3182c0bbb6
Jay Dey <cs100vaj@ieng6-202.ucsd.edu>
2013-08-28 14:43:41 -0700

    Reduced compilation errors

--- P3/HCTree.cpp
+++ P3/HCTree.cpp
@@ -1,5 +1,6 @@
 #include "HCTree.hpp"
 #include <queue>
+#include <string>
 
 /** P3
  *  Implementation of the HCTree class
@@ -10,13 +11,13 @@
 
 void HCTree::build(const vector<int>& freqs) {
   int i;
-  for (i = 0; i < 256, i++){ //convert vector to HCNodes
+  for (i = 0; i < 256; i++){ //convert vector to HCNodes
     HCNode* temp = new HCNode(freqs[i], i, 0, 0, 0);
     leaves[i] = temp; //create a new vector to look up the pointers
                       //for those HCNodes based on bit value
   }
 
-  std::priority_queue< HCNode, leaves, HCNodePtrComp> pQueue;
+  priority_queue< HCNode, leaves, HCNodePtrComp> pQueue;
 
   while(pQueue.size() != 1){
     HCNode* temp1 = pQueue.top();
@@ -32,8 +33,23 @@ void HCTree::build(const vector<int>& freqs) {
 
 }
 
+string encodeHelper(byte symbol){
+  HCNode* temp = leaves[symbol];
+  string sTemp;
+  while (temp->p){
+  if(temp == temp->p->c0){
+    sTemp.append("0");
+  }
+  else{
+    sTemp.append("1");
+  }
+  temp = temp->p;
+  }
+  return sTemp;
+}
+
 void HCTree::encode(byte symbol, BitOutputStream& out) const {
-  std::string str = encodeHelper(symbol);
+  string str = encodeHelper(symbol);
   int i;
   for (i = 0; i<str.size(); i++){
     if (str[i] == 1){
@@ -45,25 +61,11 @@ void HCTree::encode(byte symbol, BitOutputStream& out) const {
   }
 }
 
-char * HCTree::encodeHelper(byte symbol){
-  HCNode* temp = leaves[symbol];
-  std::string sTemp;
-  while (temp->p){
-  if(temp == temp->p->c0){
-    sTemp::append("0");
-  }
-  else{
-    sTemp::append("1");
-  }
-  temp = temp->p;
-  }
-  return sTemp;
-}
 
 int HCTree::decode(BitInputStream& in) const {
   HCNode* temp = root;
   int bit;
-  while ((bit = in.readbit()) != NULL){
+  while (bit = in.readBit()){
     if ((temp->c0 != NULL) || (temp->c1 != NULL)){
       if (bit == 1){
         temp = temp->c1;
@@ -73,7 +75,7 @@ int HCTree::decode(BitInputStream& in) const {
       }
     }
     else {
-      return temp->symbol
+      return temp->symbol;
     }
   }
 }
